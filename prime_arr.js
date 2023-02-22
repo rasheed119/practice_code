0c8b6f951ff211483a56391bc28a3a05237729bd
rasheed119 <78138853+rasheed119@users.noreply.github.com>
2023-02-22 10:24:33 +0530

    Add files via upload

--- prime_arr.js
+++ prime_arr.js
@@ -1,9 +1,9 @@
-var arr = [56,45,89,78,98,25,32,12];
-var count = 0;
-var prime = [];
-var max_value = Math.max(...arr);
-for(var i = 0 ; i <= arr.length ; i++){
-    for(var j = 1 ; j <= max_value ; j++){
+const prime_value = function (arr){
+    var count = 0;
+    var prime = [];
+    var max_value = Math.max(...arr);
+    for(var i = 0 ; i <= arr.length ; i++){
+      for(var j = 1 ; j <= max_value ; j++){
         if(arr[i]%j===0){
             count++;
         }
@@ -12,5 +12,8 @@ for(var i = 0 ; i <= arr.length ; i++){
         prime.push(arr[i]);
     }
     count = 0;
+    }
+    return prime;
 }
-console.log(prime);
+var arr = [56,45,89,78,98,25,32,12];
+console.log(prime_value(arr));
